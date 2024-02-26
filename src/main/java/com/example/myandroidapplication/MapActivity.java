935ef7515eb2ae43849751d1eda300ea1b0a0359
smhasan20 <120569651+smhasan20@users.noreply.github.com>
2024-02-26 20:47:23 +0600

    Add files via upload

---
+++ src/main/java/com/example/myandroidapplication/MapActivity.java
@@ -0,0 +1,34 @@
+package com.example.myandroidapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap gMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        LatLng location = new LatLng(22.3752, 91.8349);
+        googleMap.addMarker(new MarkerOptions().position(location).title("Chittagong"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,12));
+    }
+}
